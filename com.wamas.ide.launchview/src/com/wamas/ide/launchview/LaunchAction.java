9fa6682a337e9b09ba4f26cb8b610f10856ff2f3
Markus Duft <markus.duft@ssi-schaefer.com>
2017-03-07 07:27:43 +0100

    fix enablement of actions

--- com.wamas.ide.launchview/src/com/wamas/ide/launchview/LaunchAction.java
+++ com.wamas.ide.launchview/src/com/wamas/ide/launchview/LaunchAction.java
@@ -3,10 +3,13 @@
  */
 package com.wamas.ide.launchview;
 
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.debug.core.ILaunchMode;
 import org.eclipse.jface.action.Action;
 
@@ -14,15 +17,15 @@ import com.wamas.ide.launchview.model.LaunchObjectModel;
 
 public class LaunchAction extends Action {
 
-	private static final Map<String, String> COMMON_MODE_ICONS;
-	
-	static {
-		COMMON_MODE_ICONS = new TreeMap<>();
-		COMMON_MODE_ICONS.put("run", "icons/launch_run.gif");
-		COMMON_MODE_ICONS.put("debug", "icons/debug_exc.gif");
-		COMMON_MODE_ICONS.put("profile", "icons/profile_exc.gif");
-	}
-	
+    private static final Map<String, String> COMMON_MODE_ICONS;
+
+    static {
+        COMMON_MODE_ICONS = new TreeMap<>();
+        COMMON_MODE_ICONS.put("run", "icons/launch_run.gif");
+        COMMON_MODE_ICONS.put("debug", "icons/debug_exc.gif");
+        COMMON_MODE_ICONS.put("profile", "icons/profile_exc.gif");
+    }
+
     private final Set<LaunchObjectModel> models;
     private final ILaunchMode mode;
 
@@ -31,10 +34,26 @@ public class LaunchAction extends Action {
         this.models = models;
 
         setText(mode.getLabel() + " selected");
-        
-        if(COMMON_MODE_ICONS.containsKey(mode.getIdentifier())) {
-        	setImageDescriptor(Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, COMMON_MODE_ICONS.get(mode.getIdentifier())));
+
+        if (COMMON_MODE_ICONS.containsKey(mode.getIdentifier())) {
+            setImageDescriptor(
+                    Activator.imageDescriptorFromPlugin(Activator.PLUGIN_ID, COMMON_MODE_ICONS.get(mode.getIdentifier())));
         }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return models.stream().allMatch((m) -> {
+            if (m.getObject() == null) {
+                return false;
+            }
+            try {
+                return m.getObject().getType().getDelegates(Collections.singleton(mode.getIdentifier())).length > 0;
+            } catch (CoreException e) {
+                Activator.log(IStatus.WARNING, "problem retreiving launch delegates", e);
+                return false;
+            }
+        });
     }
 
     @Override
